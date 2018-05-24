01bf71f9ad1eeda623ad53bd1721b61842481028
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-05-24 15:50:34 -0400

    First Commit

---
+++ djorg/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\knoxv\desktop\lambda\(20)dj~1\bookma~1\djorg\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
