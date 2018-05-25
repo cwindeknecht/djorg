de5c96e5d8c5f7855999c5aedc963848c17e2883
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-05-25 17:53:40 -0400

    Forgot to save...

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-# from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('', views.index, name='index'),
     path('login/', views.login_user, name='login')
-    # path('login/', auth_views.LoginView.as_view()),
 ]
