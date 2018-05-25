18809b83c25d1fd50c05e557fa1d09732530edf2
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-05-25 17:52:39 -0400

    Updated Login
    Now to try Heroku Again...

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+# from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('', views.index, name='index'),
     path('login/', views.login_user, name='login')
+    # path('login/', auth_views.LoginView.as_view()),
 ]
--- djorg/urls.py
+++ djorg/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html')),
-    path('login/', auth_views.LoginView.as_view()),
+    # path('login/', auth_views.LoginView.as_view()),
     path('bookmarks/', include('bookmarks.urls')),
     path('admin/', admin.site.urls),
 ]
